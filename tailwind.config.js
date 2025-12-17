622053c32bbeebd4aabb948f4f51bc12f0ef329e
Dushyal <dhotedushyal1999@gmail.com>
2025-12-17 17:14:37 +0530

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./app/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
